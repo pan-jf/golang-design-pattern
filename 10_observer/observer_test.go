5af770cbdb92b108a9ff22b64832e84ccacc40ba
pan-jf <1274196293@qq.com>
2023-05-29 15:31:01 +0800

    观察者模式调试

--- 10_observer/observer_test.go
+++ 10_observer/observer_test.go
@@ -1,15 +1,21 @@
 package observer
 
-func ExampleObserver() {
+import "testing"
+
+func TestExampleObserver(t *testing.T) {
+	// 被观察者
 	subject := NewSubject()
+	// 观察者
 	reader1 := NewReader("reader1")
 	reader2 := NewReader("reader2")
 	reader3 := NewReader("reader3")
+	// 注册监听
 	subject.Attach(reader1)
 	subject.Attach(reader2)
 	subject.Attach(reader3)
-
+	// 被观察者发生改变
 	subject.UpdateContext("observer mode")
+
 	// Output:
 	// reader1 receive observer mode
 	// reader2 receive observer mode
